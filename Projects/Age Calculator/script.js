40e1422b8a85fb68b4bc59fc26cf4af3ccfade59
Hopper911 <78944056+hopper911@users.noreply.github.com>
2023-11-07 19:24:14 -0500

    Update script.js
    The calculateAge function now uses date subtraction to find the time difference, which it then converts to a date object (age), from which it extracts the years, months, and days.
    
    The years are corrected by subtracting the epoch year (1970) since 'getUTCFullYear' returns the number of years since 1970.
    
    The days are decremented by 1 because the getUTCDate returns the day of the month which starts at 1, not the total days difference.
    
    Removed the 'getDayInMonth' function since it's no longer necessary with this approach.
    
    This approach assumes that the 'userInput' contains a valid date. Additional validation might be necessary in a production environment.

--- Projects/Age Calculator/script.js
+++ Projects/Age Calculator/script.js
@@ -4,40 +4,14 @@ let result = document.getElementById('result');
 
 function calculateAge(){
     let birthDate = new Date(userInput.value);
-
-    let d1 = birthDate.getDate();
-    let m1 = birthDate.getMonth()+1;
-    let y1 = birthDate.getFullYear();
-
     let today = new Date();
+    let age = new Date(today - birthDate);
 
-    let d2 = today.getDate();
-    let m2 = today.getMonth() + 1;
-    let y2 = today.getFullYear();
+    let years = age.getUTCFullYear() - 1970; // Subtract the epoch year
+    let months = age.getUTCMonth();
+    let days = age.getUTCDate() - 1; // Subtract 1 day since the day of the month starts from 1
 
-    let d3,m3,y3;
-
-    y3 = y2 - y1;
-
-    if(m2 >= m1){
-        m3 = m2 - m1;
-    }
-    else{
-        y3--;
-        m3 = 12 + m2 - m1;
-    }
-    if(d2>=d1){
-        d3 = d2 - d1;
-    }
-    else{
-        m3--;
-        d3 = getDayInMonth(y1,m1)+d2-d1;
-    }
-    if(m3<0){
-        m3 = 11;
-        y3--;
-    }
-    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+    result.innerHTML = `You are ${years} years, ${months} months and ${days} days old`;
 }
 
 function getDayInMonth(year,month){
